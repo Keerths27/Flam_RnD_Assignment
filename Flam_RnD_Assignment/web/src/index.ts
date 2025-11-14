d7ea950555d62e9eade4169ce2e49977c42a4995
Keerthana B <167963515+Keerths27@users.noreply.github.com>
2025-11-14 10:21:27 +0530

    Update stats info in index.ts

--- Flam_RnD_Assignment/web/src/index.ts
+++ Flam_RnD_Assignment/web/src/index.ts
@@ -1,9 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
   const stats = document.getElementById("stats") as HTMLDivElement;
+
   const info = [
-    "Resolution: 1280x720",
-    "Approx FPS: 15",
-    "Processing: Edge detection (Canny in native C++)"
+    "Sample processed frame: processed_frame.png",
+    "Resolution: 640x480 (grayscale)",
+    "Processing: Canny edge detection in native C++ using OpenCV"
   ];
+
   stats.innerText = info.join(" | ");
 });
